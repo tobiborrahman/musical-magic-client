64d0299b002326d49a9178837c06bdb1db0cbfb1
TobiborRahman <r.tobibor16@gmail.com>
2023-06-10 10:58:23 +0600

    payment succeeded

--- src/Payments/Checkout.jsx
+++ src/Payments/Checkout.jsx
@@ -1,20 +1,28 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React, { useEffect, useState } from 'react';
 import useAxiosSecure from '../hooks/useAxiosSecure';
+import useAuth from '../hooks/useAuth';
 
 const Checkout = ({ price }) => {
 	const stripe = useStripe();
 	const elements = useElements();
+	const { user } = useAuth();
 	const [axiosSecure] = useAxiosSecure();
 	const [cardError, setCardError] = useState();
 	const [clientSecret, setClientSecret] = useState('');
+	const [processing, setProcessing] = useState(false);
 
 	useEffect(() => {
-		axiosSecure.post('/payment-intent-method', { price }).then((res) => {
-			console.log(res.data.clientSecret);
-			setClientSecret(res.data.clientSecret);
-		});
-	}, []);
+		console.log('from checkout', price);
+		if (price > 0) {
+			axiosSecure
+				.post('/payment-intent-method', { price })
+				.then((res) => {
+					console.log(res.data.clientSecret);
+					setClientSecret(res.data.clientSecret);
+				});
+		}
+	}, [price]);
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
@@ -39,8 +47,30 @@ const Checkout = ({ price }) => {
 			console.log('error', error);
 			setCardError(error.message);
 		} else {
-			console.log('payment method', paymentMethod);
+			// console.log('payment method', paymentMethod);
 			setCardError('');
+		}
+
+		setProcessing(true);
+
+		const { paymentIntent, error: confirmError } =
+			await stripe.confirmCardPayment(clientSecret, {
+				payment_method: {
+					card: card,
+					billing_details: {
+						email: user?.email || 'anonymous user',
+						name: user?.displayName || 'anonymous user',
+					},
+				},
+			});
+
+		if (confirmError) {
+			console.log(confirmError);
+		}
+		console.log(paymentIntent);
+
+		if (paymentIntent.status === 'succeeded') {
+			const transactionId = paymentIntent.id;
 		}
 	};
 
--- src/Payments/Payments.jsx
+++ src/Payments/Payments.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../components/SectionTitle';
 import { Elements } from '@stripe/react-stripe-js';
 import Checkout from './Checkout';
@@ -10,7 +10,8 @@ const Payments = () => {
 	const [selectedClasses, setSelectedClasses] = useState([]);
 
 	const total = selectedClasses?.reduce((sum, item) => item.price + sum, 0);
-	const price = parseFloat(total.toFixed(2));
+	const price = Math.round(total.toFixed(2));
+	console.log('from payment', price);
 
 	useEffect(() => {
 		fetch('http://localhost:5000/class')
@@ -23,11 +24,13 @@ const Payments = () => {
 
 	return (
 		<div className="w-3/4">
-			<div>
-				<SectionTitle
-					subHeading="Process"
-					heading="Payment"
-				></SectionTitle>
+			<div className="mt-10">
+				{/* <div>
+					<SectionTitle
+						subHeading="Process"
+						heading="Payment"
+					></SectionTitle>
+				</div> */}
 
 				<Elements stripe={stripePromise}>
 					<Checkout price={price}></Checkout>
