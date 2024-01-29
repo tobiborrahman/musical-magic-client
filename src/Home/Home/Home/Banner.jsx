40ae3136876296a3398bbc6bae92bd0986fb4f45
Tobibor Rahman <r.tobibor16@gmail.com>
2024-01-29 08:41:20 +0600

    banner updated

--- src/Home/Home/Home/Banner.jsx
+++ src/Home/Home/Home/Banner.jsx
@@ -7,8 +7,8 @@ import 'swiper/css';
 
 const Banner = () => {
 	return (
-		<div className="custom-container">
-			<div className="container">
+		<div className="">
+			<div className="">
 				<Swiper
 					slidesPerView={1}
 					autoplay={{
@@ -24,22 +24,25 @@ const Banner = () => {
 							src="https://i.ibb.co/qMHtPbd/aliane-schwartzhaupt-Qdm-Uva6q-BTk-unsplash.jpg"
 							alt="Image 1"
 						/>
-						<div className="absolute top-[37%]   text-center">
-							<div>
-								<h1 className="md:text-7xl text-3xl pb-3 font-bold text-[#EFCF4F]">
-									Let Life Be Tuneful
-								</h1>
-								<p className="text-white md:text-center text-justify md:px-56 px-6">
-									Learning a musical instrument also fosters a
-									sense of accomplishment and confidence as
-									skills improve, and can create opportunities
-									for personal growth, performance, and
-									collaboration with other musicians.
-								</p>
+						<div>
+							<div className="absolute top-[37%]  2xl:left-[24%]  text-center">
+								<div className="custom-container">
+									<h1 className="md:text-7xl text-3xl pb-3 font-bold text-[#EFCF4F]">
+										Let Life Be Tuneful
+									</h1>
+									<p className="text-white md:text-center text-justify md:px-56 px-6">
+										Learning a musical instrument also
+										fosters a sense of accomplishment and
+										confidence as skills improve, and can
+										create opportunities for personal
+										growth, performance, and collaboration
+										with other musicians.
+									</p>
+								</div>
 							</div>
 						</div>
 					</SwiperSlide>
-					<SwiperSlide>
+					{/* <SwiperSlide>
 						<img
 							className="w-full h-[600px]"
 							src="https://i.ibb.co/VpMj7Bh/saxophone.jpg"
@@ -76,7 +79,7 @@ const Banner = () => {
 								with other musicians.
 							</p>
 						</div>
-					</SwiperSlide>
+					</SwiperSlide> */}
 				</Swiper>
 			</div>
 		</div>
--- src/Home/Home/Swiper/Swiper.jsx
+++ src/Home/Home/Swiper/Swiper.jsx
@@ -9,52 +9,54 @@ const Swiper = () => {
 				subHeading="Gallery"
 				heading="Popular Collection"
 			></SectionTitle>
-			<Fade duration={3000}>
-				<div className="grid md:grid-cols-3 md:mx-24 mx-3 gap-3">
-					<div>
-						<img
-							className="md:w-[350px] w-full h-[250px]"
-							src="https://i.ibb.co/T4vWvYh/saxophonist11.jpg"
-							alt=""
-						/>
+			<div className="custom-container">
+				<Fade duration={3000}>
+					<div className="grid md:grid-cols-3 md:mx-24 mx-3 gap-3">
+						<div>
+							<img
+								className="md:w-[350px] w-full h-[250px]"
+								src="https://i.ibb.co/T4vWvYh/saxophonist11.jpg"
+								alt=""
+							/>
+						</div>
+						<div>
+							<img
+								className="md:w-[350px] w-full h-[250px]"
+								src="https://i.ibb.co/hgchjsy/gutarist.jpg"
+								alt=""
+							/>
+						</div>
+						<div>
+							<img
+								className="md:w-[350px] w-full h-[250px]"
+								src="https://i.ibb.co/09B0HGj/pianist.jpg"
+								alt=""
+							/>
+						</div>
+						<div>
+							<img
+								className="md:w-[350px] w-full h-[250px]"
+								src="https://i.ibb.co/SBSNhg1/trumpetist.jpg"
+								alt=""
+							/>
+						</div>
+						<div>
+							<img
+								className="md:w-[350px] w-full h-[250px]"
+								src="https://i.ibb.co/BtrQMrT/drumist.jpg"
+								alt=""
+							/>
+						</div>
+						<div>
+							<img
+								className="md:w-[350px] w-full h-[250px]"
+								src="https://i.ibb.co/4jjyws3/violinist.jpg"
+								alt=""
+							/>
+						</div>
 					</div>
-					<div>
-						<img
-							className="md:w-[350px] w-full h-[250px]"
-							src="https://i.ibb.co/hgchjsy/gutarist.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="md:w-[350px] w-full h-[250px]"
-							src="https://i.ibb.co/09B0HGj/pianist.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="md:w-[350px] w-full h-[250px]"
-							src="https://i.ibb.co/SBSNhg1/trumpetist.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="md:w-[350px] w-full h-[250px]"
-							src="https://i.ibb.co/BtrQMrT/drumist.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="md:w-[350px] w-full h-[250px]"
-							src="https://i.ibb.co/4jjyws3/violinist.jpg"
-							alt=""
-						/>
-					</div>
-				</div>
-			</Fade>
+				</Fade>
+			</div>
 		</div>
 	);
 };
